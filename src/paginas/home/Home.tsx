bc714455042d0e40575fb5d2f08d6ba249e63aeb
ji hye <jihye83@hotmail.com>
2022-11-03 10:07:10 -0300

    inicializando projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img
+        src="https://i.imgur.com/H88yIo2.png"
+        alt="imagem tela home"
+        className="img"
+      />
+    </>
+  );
+}
+
+export default Home;
