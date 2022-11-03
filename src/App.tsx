ef9fbb80b929f691a61cd66162143c2807c84e2f
tatianealves <tatianealvesdeoliveira@outlook.com.br>
2022-11-03 13:32:02 -0300

    Adicionando Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './paginas/home/Home';
 import Navbar from './components/static/navbar/Navbar';
 import Footer from './components/static/footer/Footer';
 import './App.css';
+import Login from "./paginas/login/Login";
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-        <Home />
+      <div style={{minHeight: "100vh"}}>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} /> 
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -16,9 +16,7 @@ function Login() {
                         <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type='password'fullWidth />
                         <Box marginTop={2} textAlign="center">
                             <Link to="/home" className="text-decorator-none">
-                                <Button type="submit" variant="contained" color="primary">
-                                    Logar
-                                </Button>
+                                <Button type="submit" variant="contained" color="primary">Logar</Button>
                             </Link>
                         </Box>
                     </form>
@@ -26,7 +24,7 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant="subtitle1" gutterBottom align="center">Não tem uma conta?</Typography>
                         </Box>
-                            <Typography variant="subtitle1" gutterBottom align="center" className="textos1">Cadastre-se aqui:</Typography>
+                            <Typography variant="subtitle1" gutterBottom align="center" className="textos1">Cadastre-se!</Typography>
                     </Box>
                 </Box>
             </Grid>
