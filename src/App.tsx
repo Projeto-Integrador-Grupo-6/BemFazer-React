8cd3d7970660cb660047fef9261f70fdddf40b49
ScarletMartins <scarletgam@icloud.com>
2022-11-03 12:00:31 -0300

    Adicionando footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Home from './paginas/home/Home';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
 import './App.css';
 
 function App() {
   return (
-      <Home />
+    <>
+      <Navbar />
+        <Home />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+
+}
+
+export default Login;
