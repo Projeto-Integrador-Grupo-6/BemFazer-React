c98ec65cfb4f3f903894b35581914b760a796d78
tatianealves <tatianealvesdeoliveira@outlook.com.br>
2022-11-08 14:09:58 -0300

    Adicionando funcionalidades do login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,30 +1,77 @@
-import React from 'react';
-import './Login.css';
+import React , { ChangeEvent, useState, useEffect } from 'react';
 import {Button, Grid, TextField, Typography} from "@material-ui/core";
 import {Box} from "@mui/material";
 import {countReset} from "console";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import {login} from "../../services/Service";
+import UserLogin from "../../models/UserLogin";
+import './Login.css';
+
 
 function Login() {
+
+    let history = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            foto: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>){
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+        
+            useEffect(() => {
+                if (token != '') {
+                    history('/home');
+                }
+            }, [token]);
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Usuário não encontrado. Tente novamente!');
+            }
+        }
+
+
     return (
         <Grid container direction="row" justifyContent="center" alignItems="center">
             <Grid alignItems="center" xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos1">Entrar</Typography>
-                        <TextField id="usuario" label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
-                        <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type='password'fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign="center">
-                            <Link to="/home" className="text-decorator-none">
+                            
                                 <Button type="submit" variant="contained" color="primary">Logar</Button>
-                            </Link>
+                            
                         </Box>
-                    </form>
-                    <Box display="flex" justifyContent="center" marginTop={2}>
+                        </form>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
-                            <Typography variant="subtitle1" gutterBottom align="center">Não tem uma conta?</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                            <Typography variant="subtitle1" gutterBottom align="center" className="textos1">Cadastre-se!</Typography>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
+
                     </Box>
                 </Box>
             </Grid>
