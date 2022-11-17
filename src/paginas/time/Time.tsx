d4b6713c64097a06e99a5a3cb85eca1af837d514
techchoi <ninahjc.94@gmail.com>
2022-11-17 20:53:13 -0300

    estilizando o sobre o time

--- src/paginas/time/Time.tsx
+++ src/paginas/time/Time.tsx
@@ -1,130 +1,185 @@
-import React from 'react';
-import { Avatar, Stack , Box } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import './Time.css';
-import { Link } from 'react-router-dom';
-import { GitHub } from '@material-ui/icons';
-
-
+import React from "react";
+import { Avatar, Stack, Box } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "./Time.css";
+import { Link } from "react-router-dom";
+import { GitHub } from "@material-ui/icons";
 
 function Time() {
   return (
-    <div>
-      <Box display='flex'>
-      <div className="card-time">
-  <div className="img-time">
-    <img src="https://i.imgur.com/rW7epEq.jpg" alt="Foto de uma pessoa" />
-  </div>
-  <span className="heading">Jihye Koo</span>
-  <p className="texto-card">
-    <a target='_blank' href='https://github.com/jihye83'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://www.linkedin.com/in/jihye-koo/' >
-    <LinkedInIcon />
-   </a>
-  </p>
-</div>
+    <div className="dive-time">
+      <Box className="box-time" display="flex">
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/rW7epEq.jpg"
+              alt="Foto de uma pessoa"
+            />
+          </div>
+          <span className="heading">Jihye Koo</span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a target="_blank" href="https://github.com/jihye83">
+              <GitHubIcon />
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/jihye-koo/">
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
 
-<div className="card-time">
-  <div className="img-time">
-  <img src="https://i.imgur.com/mAvT9dq.jpg" alt="Foto de mulher branca com cabelos castanhos escuro" />
-  </div>
-  <span className="heading">Scarlet Martins</span>
-  <p className="texto-card">
-    <a target='_blank' href='https://github.com/ScarletMartins'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://www.linkedin.com/in/scarletmartins/' >
-   <LinkedInIcon />
-   </a>
-  </p>
-</div>
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/mAvT9dq.jpg"
+              alt="Foto de mulher branca com cabelos castanhos escuro"
+            />
+          </div>
+          <span className="heading">Scarlet Martins</span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a target="_blank" href="https://github.com/ScarletMartins">
+              <GitHubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/scarletmartins/"
+            >
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
 
-<div className="card-time">
-  <div className="img-time">
-  <img src="https://i.imgur.com/9BKUXJT.png" alt="Foto de uma menina coreana" />
-  </div>
-  <span className="heading">Nina Choi</span>
-  <p className="texto-card">
-    <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91' >
-   <LinkedInIcon />
-   </a>
-  </p>
-</div>
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/9BKUXJT.png"
+              alt="Foto de uma menina coreana"
+            />
+          </div>
+          <span className="heading">Nina Choi</span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
 
-<div className="card-time">
-  <div className="img-time">
-  <img src="https://i.imgur.com/ihhDc1E.jpg" alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto" />
-  </div>
-  <span className="heading">Tatiane Alves</span>
-  <p className="texto-card">
-    <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91' >
-   <LinkedInIcon />
-   </a>
-  </p>
-</div>
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/ihhDc1E.jpg"
+              alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto"
+            />
+          </div>
+          <span className="heading">Tatiane Alves</span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
       </Box>
 
-      <Box display='flex'>
-      <div className="card-time">
-  <div className="img-time">
-  <img src="https://i.imgur.com/ihhDc1E.jpg" alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto" />
-  </div>
-  <span className="heading">Victória </span>
-  <p className="texto-card">
-    <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91' >
-   <LinkedInIcon />
-   </a>
-  </p>
-</div>
+      <Box className="box-time2" display="flex">
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/ihhDc1E.jpg"
+              alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto"
+            />
+          </div>
+          <span className="heading">Victória Mello </span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
 
-<div className="card-time">
-  <div className="img-time">
-  <img src="https://i.imgur.com/ihhDc1E.jpg" alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto" /> 
-  </div>
-  <span className="heading">Hellen Digramont</span>
-  <p className="texto-card">
-    <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91' >
-   <LinkedInIcon />
-   </a>
-  </p>
-</div>
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/kZakBnw.jpg"
+              alt="Foto de uma mulher branca com olhos castanhos escuros e cabelos longos cacheados"
+            />
+          </div>
+          <span className="heading">Hellen Digramont</span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
 
-<div className="card-time">
-  <div className="img-time">
-  <img src="https://i.imgur.com/ihhDc1E.jpg" alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto" /> 
-  </div>
-  <span className="heading">Isabela Soares</span>
-  <p className="texto-card">
-    <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91'>
-    <GitHubIcon />
-      </a>
-   <a target='_blank' href='https://uiverse.io/gagan-gv/afraid-otter-91' >
-   <LinkedInIcon />
-   </a>
-  </p>
-</div>
+        <div className="card-time">
+          <div className="img-time">
+            <img
+              src="https://i.imgur.com/ihhDc1E.jpg"
+              alt="Foto de uma mulher branca com olhos verdes e cabelos longos preto"
+            />
+          </div>
+          <span className="heading">Isabela Soares</span>
+          <p className="cargo">Desenvolvedora FullStack</p>
+          <p className="texto-card">
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://uiverse.io/gagan-gv/afraid-otter-91"
+            >
+              <LinkedInIcon />
+            </a>
+          </p>
+        </div>
       </Box>
     </div>
-
-    
-  )
+  );
 }
 
-export default Time
-
-
+export default Time;
